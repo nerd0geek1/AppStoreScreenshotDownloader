4f4d34a1ce62dae3e4bc4792a6ee31ef4559223d
Kohei Tabata <nerd0geek1@gmail.com>
2019-01-15 23:12:56 +0900

    add logic to get user input

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import * as readline from 'readline-sync';
+
+console.log('Please input appleID of iOS App which you want to download screenshots.');
+console.log('>');
+const appleID = readline.question();
